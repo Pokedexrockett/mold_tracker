cbdc2fcc0013df42a3269f315b792c579ebb94fd
Pokedexrockett <dex429.dc@gmail.com>
2022-03-15 20:19:15 -0400

    make sure all up to date

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ApplicationRecord
     has_many :strains
     has_many :notes
-    has_many :strain_notes, through: :notes, source: :strain
+    has_many :noted_strains, through: :notes, source: :strain
     has_many :dry_rooms
 
     has_secure_password
